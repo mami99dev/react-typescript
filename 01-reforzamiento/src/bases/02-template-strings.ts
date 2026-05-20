51b0d22c9a1ff61ae486a64f08886a32e3b1f079
mami-caprepol <isaacmar24@gmail.com>
2026-05-20 12:15:55 -0600

    template strings

---
+++ 01-reforzamiento/src/bases/02-template-strings.ts
@@ -0,0 +1,6 @@
+const firstName = 'Isaac'
+const lastName = "O'Neal"
+
+const fullName = `${firstName} ${lastName}` // Al usar las template strings, se pueden incluir variables dentro de la cadena de texto utilizando la sintaxis ${variable}. En este caso, se crea una nueva cadena de texto que combina el valor de firstName y lastName con un espacio entre ellos.
+
+console.log({firstName, lastName, fullName})
