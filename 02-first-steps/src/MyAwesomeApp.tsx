b6d5217f28ec93998f5e2fe0f5af638f2f2a5fa1
mami-caprepol <isaacmar24@gmail.com>
2026-05-25 13:01:26 -0600

    print variables in a component

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,27 @@
+const firstName = 'Isaac'
+const lastName = 'Martinez'
+
+const favoriteSports = ['Soccer', 'Ping pong', 'Swimming']
+const isActive = true
+
+const address = {
+  country: 'MX',
+  zipCode: '54080'
+}
+
 export function MyAwesomeApp() {
   return (
     <>
-      <h1>Isaac</h1>
-      <h3>Martinez</h3>
+      <h1>{firstName}</h1>
+      <h3>{lastName}</h3>
+
+      <p>{favoriteSports}</p>
+      <p>{favoriteSports.join(', ')}</p>
+
+      <h1>{isActive}</h1>
+      <h2>{isActive ? 'Activo' : 'No activo'}</h2>
+
+      <p>{JSON.stringify(address)}</p>
     </>
   )
 }
