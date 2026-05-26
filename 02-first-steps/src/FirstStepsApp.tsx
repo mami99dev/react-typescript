0d1471d234e4d5fd7ab0611c01acc3ed14294e44
mami-caprepol <isaacmar24@gmail.com>
2026-05-26 11:48:00 -0600

    render elements by an array with map method

--- 02-first-steps/src/FirstStepsApp.tsx
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -1,13 +1,30 @@
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+interface ItemInCart {
+  productName: string;
+  quantity: number;
+}
+
+const itemsInCart: ItemInCart[] = [
+  { productName: 'Nintendo Switch 2', quantity: 1 },
+  { productName: 'Pro controller', quantity: 4 },
+  { productName: 'Super Smash', quantity: 2 },
+]
+
 export function FirstStepsApp() {
   return (
     <>
       <h1>Carrito de compras</h1>
+      {
+        itemsInCart.map(({ productName, quantity }) => (
+          // Se requiere agregar un key cada que se renderiza un ItemCounter
+          <ItemCounter key={productName} name={productName} quantity={quantity} /> //No usar index como el valor de key
+        ))
+      }
 
-      <ItemCounter name='Nintendo Switch 2' quantity={1} />
+      {/* <ItemCounter name='Nintendo Switch 2' quantity={1} />
       <ItemCounter name='Pro controller' quantity={2} />
-      <ItemCounter name='Super Smash' quantity={1} />
+      <ItemCounter name='Super Smash' quantity={1} /> */}
     </>
   )
 }
