be3f126a320ac6de96425af4cee40472d5797fc8
mami-caprepol <isaacmar24@gmail.com>
2026-05-29 13:12:36 -0600

    testing render the correct number of ItemCounter components and with correct props it

--- 02-first-steps/src/FirstStepsApp.test.tsx
+++ 02-first-steps/src/FirstStepsApp.test.tsx
@@ -1,24 +1,49 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, test, vi } from "vitest";
+import { afterEach, describe, expect, test, vi } from "vitest";
 import { FirstStepsApp } from "./FirstStepsApp";
 
-//! los Mocks se usan para simular el comportamiento de un componente o módulo, permitiendo controlar su salida y evitar efectos secundarios no deseados durante las pruebas.
+const mockItemCounter = vi.fn((props: unknown) => {
+  return (
+    <div data-testid="ItemCounter" />
+  )
+})
+
 vi.mock('./shopping-cart/ItemCounter', () => ({
-  ItemCounter: () => <div data-testid="ItemCounter" /> // Esto hace que cada vez que se renderice el componente ItemCounter en FirstStepsApp, se renderice un div con el atributo data-testid="ItemCounter" en su lugar. Esto nos permite contar cuántas veces se renderiza el componente ItemCounter sin preocuparnos por su implementación interna o efectos secundarios.
-})) // Mockeamos el componente ItemCounter para evitar que se renderice su contenido y así poder probar el número de veces que se renderiza en FirstStepsApp
+  ItemCounter: (props: unknown) => mockItemCounter(props)
+}))
 
 describe('FirstStepsApp', () => {
+  //! Existen funciones que se ejecutan antes o después de cada test, o de toda la suite de tests
+  afterEach(() => {
+    vi.clearAllMocks() // Limpia el estado de los mocks después de cada test, para evitar que las llamadas a los mocks se acumulen entre tests
+  })
+
   test('Should match snapshot', () => {
     const { container } = render(<FirstStepsApp />)
-    expect(container).toMatchSnapshot() // En el snapshot se mostraran los ItemCounter como divs con el atributo data-testid="ItemCounter" en lugar de su contenido real, lo que nos permite verificar que se renderizan correctamente sin preocuparnos por su implementación interna.
+    expect(container).toMatchSnapshot()
   })
 
   test('Should render the correct number of ItemCounter components', () => {
     render(<FirstStepsApp />)
 
-    const itemCounters = screen.getAllByTestId('ItemCounter') // Obtenemos todos los elementos que tienen el atributo data-testid="ItemCounter" que ya ha sido mockeado, lo que nos permitirá contar cuántas veces se renderiza el componente ItemCounter en FirstStepsApp
+    const itemCounters = screen.getAllByTestId('ItemCounter')
 
-    expect(itemCounters.length).toBe(3) // Esperamos que se rendericen 3 componentes ItemCounter
+    expect(itemCounters.length).toBe(3)
+  })
+
+  test('Should render ItemCounter with correct props', () => {
+    render(<FirstStepsApp />)
+
+    expect(mockItemCounter).toHaveBeenCalledTimes(3) // Verifica que el mock de ItemCounter se haya llamado exactamente 3 veces, lo que indica que se renderizaron 3 componentes ItemCounter
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      name: 'Nintendo Switch 2', quantity: 1
+    }) // Verifica que el mock de ItemCounter se haya llamado con las props correctas para el primer producto
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      name: 'Pro controller', quantity: 4
+    }) // Verifica que el mock de ItemCounter se haya llamado con las props correctas para el segundo producto
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      name: 'Super Smash', quantity: 2
+    }) // Verifica que el mock de ItemCounter se haya llamado con las props correctas para el tercer producto
   })
 
 })
