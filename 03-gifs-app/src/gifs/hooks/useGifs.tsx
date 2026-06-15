4b044b548caac172eb2ab50c500f85b2b49c2767
Isaac Martinez <isaacmar99@icloud.com>
2026-06-15 02:23:07 -0600

    avoid making http requests with cache memory management

--- 03-gifs-app/src/gifs/hooks/useGifs.tsx
+++ 03-gifs-app/src/gifs/hooks/useGifs.tsx
@@ -2,12 +2,20 @@ import { useState } from "react"
 import type { Gif } from "../interfaces/gif.interface"
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action"
 
+// Se declara afuera del componente ya que asi evitamos la perdida de informacion en el re-render del componente
+const gifsCache: Record<string, Gif[]> = {}
+
 export const useGifs = () => {
   const [previousTerms, setPreviousTerms] = useState<string[]>([])
   const [gifs, setGifs] = useState<Gif[]>([])
 
   const handleTermClicked = async (term: string) => {
-    // No es óptimo porque no tiene caché
+    // Si existe en memoria entonces ya no hace la peticion http
+    if (gifsCache[term]) {
+      setGifs(gifsCache[term])
+      return
+    }
+
     const searchedGifs = await getGifsByQuery(term)
     setGifs(searchedGifs)
   }
@@ -23,6 +31,8 @@ export const useGifs = () => {
 
     const searchedGifs = await getGifsByQuery(query)
     setGifs(searchedGifs)
+
+    gifsCache[query] = searchedGifs
   }
 
   return {
